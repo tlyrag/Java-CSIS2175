63ec0e824b333994be08d06ad5ba218a8af5b4dd
Thiago Lyra Ganem <tlyraganem@gmail.com>
2023-06-15 14:39:05 -0700

    Palindrome checker

---
+++ Assignment2/src/Palindrome.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Palindrome {
+	public static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Geting user input
+		String userInput = getUserInput();
+		
+		//Transforming to Array and building the String with letters only
+		ArrayList<Character> lettersInUserInput = getLettersInPhrase(userInput);
+		String userPhraseLetters = buildString(lettersInUserInput).toString();
+		
+		// Reversing the array and building the String with letters only
+		ArrayList<Character> reverseLettersInUserInput = reverseArray(lettersInUserInput);
+		String userReversePhraseLetters = buildString(reverseLettersInUserInput).toString();
+		
+		//Checking if it is Palindrome
+		
+		palindromeChecker(userPhraseLetters,userReversePhraseLetters);
+		
+		
+	}
+	public static ArrayList<Character> getLettersInPhrase(String phrase) {
+		
+		ArrayList<Character> charList  = new ArrayList<Character>();
+		char[] phraseArray = phrase.toCharArray();
+		for(int i =0;i<phraseArray.length;i++) {
+			
+			if(Character.isLetter(phraseArray[i])){
+				charList.add(phraseArray[i]);
+			} 
+		}
+		return charList;
+	}
+	public static ArrayList<Character> reverseArray(ArrayList<Character> charList) {
+		
+		ArrayList<Character> reversedCharList  = new ArrayList<Character>();
+		
+		for(int i=0;i<charList.size();i++) {
+			reversedCharList.add(0, charList.get(i));
+		}
+		return reversedCharList;
+	}
+	public static StringBuilder buildString(ArrayList<Character> charList) {
+		StringBuilder newString = new StringBuilder();
+		for(int i=0;i<charList.size();i++) {
+			newString.append(charList.get(i));
+		}
+		
+		return newString;
+	}
+	public static boolean palindromeChecker(String str1,String str2) {
+		
+		if(str1.toLowerCase().equals(str2.toLowerCase())) {
+			System.out.println(str1.toLowerCase()+ " Is a palindrome phrase to "+ str2.toLowerCase());
+			return true;
+		} else {
+			System.out.println(str1.toLowerCase()+ " Is not a palindrome phrase to "+ str2.toLowerCase());
+			return false;
+		} 
+	}
+	public static String getUserInput() {
+			System.out.println("Please enter you phrase");
+			String userInput =input.nextLine();
+			return userInput;
+		
+	}
+
+}
+ 
--- Assignment2/src/Perfect.java
+++ Assignment2/src/Perfect.java
@@ -1,9 +1,41 @@
-
+import java.util.ArrayList;
 public class Perfect {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Integer> perfectNumList = new ArrayList<Integer>();
+		
+		for(int i = 1;i<=1000;i++) {
+			if(i == getListSum(getEvenlyDivedNumbers(i))) {
+				perfectNumList.add(i);
+			}
+		}
+		//displayList(perfectNumList);
+		System.out.println(perfectNumList);
 	}
-
+	
+	public static ArrayList<Integer> getEvenlyDivedNumbers(int num) {
+		ArrayList<Integer> evenlyList = new ArrayList<Integer>();
+		
+		for(int i = 1; i<num;i++) {
+			if(num%i==0) {
+				evenlyList.add(i);
+			}
+		}
+		return evenlyList;
+	}
+	
+	public static int getListSum(ArrayList<Integer> list) {
+		int sumNumber=0;
+		
+		for(int i = 0; i<list.size();i++) {
+			sumNumber+=list.get(i);
+		}
+		return sumNumber;
+	}
+	public static void displayList(ArrayList<Integer> list) {
+		for(int i =0; i<list.size();i++) {
+			System.out.println(list.get(i));
+		}
+	}
 }
